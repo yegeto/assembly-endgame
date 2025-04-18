da8689040d079ff69761b2127984721f25654242
yegeto <115154804+yegeto@users.noreply.github.com>
2025-04-18 13:14:02 +0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
-function App() {}
+function App() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Assembly: Endgame</h1>
+      <p>
+        Guess the word within 8 attempts to keep the programming world safe from
+        Assembly!
+      </p>
+    </header>
+  );
+}
